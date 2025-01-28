b9215e249a5b9d3a1d0151b4651739412821cb31
Sahno86 <mishenkinav@gmail.com>
2025-01-28 21:06:19 +0500

    lesson_15

--- lesson_14/save_cookies.py
+++ lesson_14/save_cookies.py
@@ -5,7 +5,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
 options = webdriver.ChromeOptions()
